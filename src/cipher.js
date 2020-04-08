07ffad9aa258104d0bffc6719f1c93a35e904cb7
marcellita <mteliceskidalpino@gmail.com>
2020-04-08 19:16:54 -0300

    raciocínio lógico final

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
 const cipher = {
-  // ...
+   encode: function  (offset, string){
+
+     const frase = string.toUpperCase(); 
+     const numberoffset = offset;
+     let resultado = "";
+
+    for (let i = 0; i <frase.length; i ++){
+      const posicaodaletra=frase.charCodeAt(i);
+       resultado = resultado + String.fromCharCode(((posicaodaletra - 65 + numberoffset) % 26) + 65);
+     } return resultado;
+  },
+
+  decode: function (offset, string) {
+    const frase = string.toUpperCase(); 
+     const numberoffset = offset;
+     let resultado = "";
+   
+     for (let i = 0; i <frase.length; i ++){
+      const posicaodaletra=frase.charCodeAt(i);
+      resultado = resultado + String.fromCharCode(((posicaodaletra + 65 - numberoffset) % 26) + 65);
+    }return resultado;
+  }
 };
 
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
+const botaoCifra = document.getElementById("cifra");
+const botaoDecifra = document.getElementById("decifra");
+const frase = document.getElementById("palavra");
+const numberoffset = document.getElementById("desloc");
 
-console.log(cipher);
+botaoCifra.addEventListener("click",ResultadoCifra);
+
+function ResultadoCifra(){
+   
+   document.getElementById("resultado").innerHTML = cipher.encode(numberoffset, frase);
+
+}
+
+botaoDecifra.addEventListener("click",ResultadoDecifra);
+
+function ResultadoDecifra(){
+  
+   document.getElementById("resultado").innerHTML = cipher.decode(numberoffset, frase);
+   
+}
+
+
+
