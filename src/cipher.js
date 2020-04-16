02619a83217df8273a38f45889b5203c2fe23074
marcellita <mteliceskidalpino@gmail.com>
2020-04-16 15:50:13 -0300

    atualizacao

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,25 @@
 const cipher = {
   encode: function  (offset, string){
-    if 
-     ( offset <= "0" || string == " " || typeof offset != "number" || typeof string != "string"){
-
-           throw new TypeError;
-       }
-    
-    let frase = string; 
-    let numberoffset = offset;
+    if( offset <= "0" || string === " " || typeof offset !== "number" || typeof string !== "string"){
+      throw new TypeError;
+    }
     let resultado = "";
 
-   for (let i = 0; i <frase.length; i ++){
-     let posicaodaletra= frase.charCodeAt(i);
-      resultado = resultado + String.fromCharCode(((posicaodaletra - 65 +numberoffset) % 26) + 65);
-    } return resultado;
-    
- },
- 
-
- decode: function (offset, string) {
-    let frase = string; 
-    let numberoffset = offset;
+  for (let i = 0; i <string.length; i ++){
+    let posicaodaletra = string.charCodeAt(i);
+    resultado +=String.fromCharCode(((posicaodaletra - 65 + offset) % 26) + 65);
+    } 
+  return resultado;
+},
+  decode: function (offset, string) {
     let resultado = "";
-  
-    for (let i = 0; i <frase.length; i ++){
-     let posicaodaletra=frase.charCodeAt(i);
-     resultado = resultado + String.fromCharCode(((posicaodaletra + 65 -  numberoffset) % 26) + 65);
-   }return resultado;
- }
+      for (let i = 0; i <string.length; i ++){
+    let posicaodaletra=string.charCodeAt(i);
+    resultado +=String.fromCharCode(((posicaodaletra + 65 - offset) % 26) + 65);
+  }
+  return resultado;
+}
 };
-
 export default cipher;
 
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import cipher from './cipher.js';
    let botaoCifra = document.getElementById("cifra");
+   let botaoDecifra = document.getElementById("decifra");
 
-   function ResultadoCifra(){
+   botaoCifra.addEventListener("click",resultadoCifra);
+   botaoDecifra.addEventListener("click",resultadoDecifra);
+
+   function resultadoCifra(){
    let frase = document.getElementById("palavra").value;
    let numberoffset = Number(document.getElementById("desloc").value);
    let cifragem = document.getElementById("resposta");
    cifragem.innerHTML = cipher.encode(numberoffset,frase);
-
 }
-botaoCifra.addEventListener("click",ResultadoCifra);
-
-
-let botaoDecifra = document.getElementById("decifra");
-
-function ResultadoDecifra(){
+   function resultadoDecifra(){
    let frase = document.getElementById("palavra").value;
    let numberoffset = Number(document.getElementById("desloc").value);
    let decifragem = document.getElementById("resposta");
    decifragem.innerHTML = cipher.decode(numberoffset,frase);
 }
 
-botaoDecifra.addEventListener("click",ResultadoDecifra);
 
 
 
